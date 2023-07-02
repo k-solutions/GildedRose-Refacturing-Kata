284be917e83bb8c493937d19391b59d317a5ff22
Hristo Kochev <h.l.kochev@gmail.com>
2023-07-02 22:12:07 +0300

    property testing

--- src/GildedRose.hs
+++ src/GildedRose.hs
@@ -5,6 +5,8 @@ module GildedRose
   , ValidItem
   , mkItem
   , mkInventory
+  , getQuality
+  , isItemPrefix
   )where
 
 import Data.List (isPrefixOf)
@@ -24,15 +26,28 @@ instance Show Item where
 instance  Show ValidItem where
     show (MkValidItem item) = show item
 
+getQuality :: ValidItem -> Int
+getQuality (MkValidItem (Item _ _ q)) = q
+
+isItemPrefix :: String -> ValidItem -> Bool
+isItemPrefix pref (MkValidItem (Item name _ _))
+  = pref `isPrefixOf` name
+
 mkItem :: Item -> Maybe ValidItem
 mkItem (Item name sellIn quality)
-  | "Sulfuras" `isPrefixOf` name
-  && quality /= 80  = Nothing   
+  | "Sulfuras" `isPrefixOf` name = if quality /= 80
+                                    then  Nothing
+                                    else    Just 
+                                          . MkValidItem 
+                                          . Item name sellIn 
+                                          $ 80  
   | name == ""  
   || quality < 0 
   || quality > 50 = Nothing  
-  | otherwise = Just . MkValidItem 
-              . Item name sellIn $ quality
+  | otherwise = Just 
+              . MkValidItem 
+              . Item name sellIn 
+              $ quality
 
 mkInventory :: [Item] -> GildedRose
 mkInventory = mapMaybe mkItem 
@@ -41,7 +56,7 @@ updateQuality :: GildedRose -> GildedRose
 updateQuality = map updateQualityItem
 
 updateQualityItem :: ValidItem -> ValidItem
-updateQualityItem (MkValidItem (Item name sellIn quality)) 
+updateQualityItem it@(MkValidItem (Item name sellIn quality)) 
   = case mkItem newItem of
       Nothing -> MkValidItem . Item name newSellIn $ quality
       Just it -> it  
@@ -49,17 +64,17 @@ updateQualityItem (MkValidItem (Item name sellIn quality))
     newItem = Item name newSellIn newQuality
 
     newSellIn
-     | "Sulfuras" `isPrefixOf` name = sellIn
+     | "Sulfuras" `isItemPrefix` it = sellIn
      | otherwise =  sellIn - 1        
 
     newQuality        
-      | "Sulfuras" `isPrefixOf` name = quality 
+      | "Sulfuras" `isItemPrefix` it = quality 
       | name == "Aged Brie" = quality + 1
-      | ("Backstage" `isPrefixOf` name) 
+      | ("Backstage" `isItemPrefix` it) 
       && (newSellIn < 0) = 0 
-
-      | "Backstage" `isPrefixOf` name = quality + backstageQltAdjust newSellIn 
+      | "Backstage" `isItemPrefix` it = quality + backstageQltAdjust newSellIn 
       | otherwise = quality - qltyAdjust newSellIn
+
     qltyAdjust sIn 
       | sIn < 0 = 2
       | otherwise = 1   
@@ -69,59 +84,3 @@ updateQualityItem (MkValidItem (Item name sellIn quality))
       | sIn `elem` [6..10] = 2
       | otherwise = 1   
 
---  let
---    quality' =
---      if name /= "Aged Brie"
---         && name /= "Backstage passes to a TAFKAL80ETC concert"
---      then
---        if quality > 0
---        then
---          if name /= "Sulfuras, Hand of Ragnaros"
---          then quality - 1
---          else quality
---        else quality
---      else
---        if quality < 50
---        then
---          quality + 1 +
---            (if name == "Backstage passes to a TAFKAL80ETC concert"
---             then
---               if sellIn < 11
---               then
---                 if quality < 49
---                 then
---                   1 + (if sellIn < 6
---                        then
---                          if quality < 48
---                          then 1
---                          else 0
---                        else 0)
---                 else 0
---               else 0
---             else 0)
---        else quality
---
---  in
---    case mkItem  
---      (if sellIn' < 0
---      then
---        if name /= "Aged Brie"
---        then
---          if name /= "Backstage passes to a TAFKAL80ETC concert"
---          then
-----              if quality' > 0
-----              then
---              if name /= "Sulfuras, Hand of Ragnaros"
---              then Item name sellIn' (quality' - 1)
---              else Item name sellIn' quality'
-----              else Item name sellIn' quality'
---          else Item name sellIn' (quality' - quality')
---        else
---          if quality' < 50
---          then Item name sellIn' (quality' + 1)
---          else Item name sellIn' quality'
---      else Item name sellIn' quality') 
---    of
---      Nothing -> MkValidItem . Item name sellIn' $ quality
---      Just it -> it  
---
--- test/GildedRoseSpec.hs
+++ test/GildedRoseSpec.hs
@@ -2,11 +2,50 @@ module GildedRoseSpec (spec) where
 
 import Test.Hspec
 import GildedRose
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
+import Data.Maybe (fromJust)
+
+instance Arbitrary Item where
+    arbitrary =validItem 
+instance Arbitrary ValidItem where
+    arbitrary = fromJust . mkItem <$> arbitrary
+          
+
+validQuality :: Gen Int
+validQuality = choose (0, 50)
+
+validSellIn :: Gen Int
+validSellIn = choose (-10, 60)
+
+validItem :: Gen Item
+validItem = frequency 
+          [ (1,  Item "Sulfuras, Hand of Ragnaros" <$> validSellIn <*> pure 80)
+          , (5,  Item "Backstage passes to a TAFKAL80ETC concert" <$> validSellIn <*> validQuality)
+          , (10, Item "Aged Brie" <$> validSellIn <*> validQuality)
+          , (50, Item "Foo Rose" <$> validSellIn <*> validQuality)
+          ]
+
+
+prop_MakeItem :: Item -> Bool -- Property
+prop_MakeItem item -- = collect (show item)
+  = not . null . mkInventory 
+  $  [ item ]  
+
+prop_PositiveQuality :: ValidItem -> Bool  -- Property
+prop_PositiveQuality item -- = collect (show item)
+  = getQuality (head $ updateQuality [item]) >= 0 
+
+prop_QualityMax :: ValidItem -> Property
+prop_QualityMax item 
+  =  not ("Sulfuras" `isItemPrefix` item) 
+  ==> getQuality (head $ updateQuality [ item ]) < 51
+
 
 spec :: Spec
 spec =
   describe "updateQuality" $ do
-
+    
     it "Backstage passes Quality increases by 3 when sellIn between [1...5]" $
        let inventory = mkInventory [Item "Backstage passes to a TAFKAL80ETC concert" 5 10]
            actual = updateQuality inventory
@@ -75,3 +114,9 @@ spec =
            actual = updateQuality inventory
            expected = mkInventory [Item "foo" (-1) 0]
        in actual `shouldBe` expected
+
+    prop "Generated Items are valid" prop_MakeItem
+    
+    prop "Updated quality is never negative" prop_PositiveQuality
+
+    prop "Quality has not got over Max" prop_QualityMax
